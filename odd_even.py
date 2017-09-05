f481b842aaeb143622e2c6cb04140e548f45ed82
Nick Madani <nickmadani@Nicks-MacBook-Pro.local>
2017-09-05 15:40:52 -0700

    functionfun

--- odd_even.py
+++ odd_even.py
@@ -1,3 +1,5 @@
+
+# Odd_Even - create function that counts form 1 to 2000, as loop runs, have number printed and declared of whehter it is odd or even
 def odd_even():
     num = 1
     numberis = "Number is: "
@@ -10,3 +12,11 @@ def odd_even():
         num += 1
 
 print odd_even()
+
+# Multiply - create function that iterates through each value in a list (eg a=[2,4,10,15] and returns a list where each value has been multiplied by 5)
+list1 = [3,4,5,3]
+def multiply(a,b):
+    for item in range(len(a)):
+        a[item] *= b
+    return a
+print multiply(list1, 5)
